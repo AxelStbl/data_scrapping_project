f7c8e8f5b27bd59fddc9fef6df5c7881fe9a0919
temmam <sarahtemmam@yahoo.fr>
2019-11-17 18:00:24 +0100

    I managed the potential error

--- TabScrapping.py
+++ TabScrapping.py
@@ -1,17 +1,22 @@
 import re
 from bs4 import BeautifulSoup
+import os
 
 
 def scrape_company_tab(data):
     """scrape data about companies"""
     soup = BeautifulSoup(data, 'html.parser')
     company_data = soup.find_all('div', class_="infoEntity")
+    if not company_data:
+        return None
     label_value_data = {}
     if len(company_data) > 0:
         for data in company_data:
             label_value_data[data.label.getText().lower()] = data.find('span', class_='value').get_text()
 
         span_website = soup.find('span', class_="website")
+        if not span_website:
+            return None
         if span_website:
             website_link = span_website.find('a', href=True)
             label_value_data["website"] = website_link['href']
@@ -22,6 +27,8 @@ def rating(data):
     """Returns the rating of the company"""
     soup = BeautifulSoup(data, 'html.parser')
     rate = soup.find(class_="ratingNum margRtSm").get_text()
+    if not rate:
+        return None
     return rate
 
 
@@ -29,22 +36,29 @@ def nbr_of_ratings(data):
     """Returns the number of the person who rated"""
     soup = BeautifulSoup(data, 'html.parser')
     allnbr = str(soup.find(class_="ct-bar"))
-    nbr = int(re.search(r'value="(\d*)"', allnbr).group(1))
+    if not allnbr:
+        return None
     nbr_of_ratings = []
     values = soup.find_all('line', class_="ct-bar")
+    if not values:
+        return None
     for value in values:
-        nbr = int(re.search(r'value="(\d*)"', str(value)).group(1))
+        match = re.search(r'value="(\d*)"', str(value))
+        if not match:
+            return None
+        nbr = int((match).group(1))
         nbr_of_ratings.append(nbr)
     sum = 0
     for nbr in nbr_of_ratings:
         sum += nbr
     return sum
 
-
 def benefits_rate(data):
     """Returns the rating of the benefits of the company"""
     soup = BeautifulSoup(data, 'html.parser')
     rate = soup.find(class_="ratingNum margRtSm").get_text()
+    if not rate:
+        return None
     return rate
 
 
@@ -53,10 +67,18 @@ def nbr_of_benefitsrating(data):
     """
     soup = BeautifulSoup(data, 'html.parser')
     nbr_of_ratings = soup.find(class_="minor noMargTop padSm").get_text()
+    if not nbr_of_ratings:
+        return None
     return nbr_of_ratings
 
 
 def main():
+    if not os.path.exists('Rating.html'):
+        return
+    if not os.path.exists('Benefits.html'):
+        return
+    if not os.path.exists('Company.html'):
+        return
     fd_rating = open('Rating.html', 'r')
     data_rating = fd_rating.read()
     fd_rating.close()
